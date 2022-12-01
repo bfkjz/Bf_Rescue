9ba6e154bd3730de1ead33e37b7bb1d60a95ca49
Zeb <69901066+bfkjz@users.noreply.github.com>
2022-12-01 17:16:13 +0800

    Update BfRescue.sh

--- BfRescue.sh
+++ BfRescue.sh
@@ -6,6 +6,7 @@ nmcli con add type bridge ifname bf_br0  con-name bf_Bridge0 ipv4.addresses  192
 nmcli con add type wifi ifname wlan0 con-name debianap autoconnect yes ssid debianap master bf_br0
 nmcli con modify debianap 802-11-wireless.mode ap
 nmcli con add type Ethernet ifname usb0 con-name usbEthernet  master bf_br0
+nmcli connection up usbEthernet
 nmcli connection dis USB
 echo
 echo BFrescue core OK
