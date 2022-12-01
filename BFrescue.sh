e6b570953b5aef9d06e31fc6079bc800addcf04f
Zeb <69901066+bfkjz@users.noreply.github.com>
2022-12-01 16:02:12 +0800

    Update BFrescue.sh

--- BFrescue.sh
+++ BFrescue.sh
@@ -12,9 +12,6 @@ echo
 echo -e "The system automatically restarts after \033[47;31m 5 \033[0m seconds"
 echo -e "Press \033[47;30m Ctrl + C \033[0m to cancel the restart"
 echo  
-echo -e "系统将会在 \033[47;31m 5 \033[0m 秒后自动重启"
-echo -e "按 \033[47;30m Ctrl + C \033[0m 取消重启"
-echo  
 sleep 5
 reboot
 fi
