d9786ed3b19c657f58bcec567560e0c1e652a440
Zeb <69901066+bfkjz@users.noreply.github.com>
2022-12-01 16:35:13 +0800

    Update BfRescue.sh

--- BfRescue.sh
+++ BfRescue.sh
@@ -7,14 +7,8 @@ nmcli con add type wifi ifname wlan0 con-name debianap autoconnect yes ssid debi
 nmcli con modify debianap 802-11-wireless.mode ap
 nmcli con add type Ethernet ifname usb0 con-name usbEthernet  master bf_br0
 nmcli connection down USB
+echo
 echo BFrescue core OK
 echo  
-echo -e "The system automatically restarts after \033[47;31m 5 \033[0m seconds"
-echo -e "Press \033[47;30m Ctrl + C \033[0m to cancel the restart"
-echo  
-echo -e "系统将会在 \033[47;31m 5 \033[0m 秒后自动重启"
-echo -e "按 \033[47;30m Ctrl + C \033[0m 取消重启"
-echo  
-sleep 5
-reboot
+
 fi
